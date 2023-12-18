a3fe32f7a63897fe3cb2097e45387e1830a2e7d7
Cao Viết Huy <hieppro138@gmail.com>
2023-12-19 01:23:19 +0700

    Initial Commit

---
+++ P1/P1App.swift
@@ -0,0 +1,17 @@
+//
+//  P1App.swift
+//  P1
+//
+//  Created by Cao Viet Huy on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct P1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ P1UITests/P1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  P1UITestsLaunchTests.swift
+//  P1UITests
+//
+//  Created by Cao Viet Huy on 19/12/2023.
+//
+
+import XCTest
+
+final class P1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
