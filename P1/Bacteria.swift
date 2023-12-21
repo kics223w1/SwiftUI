0599cf1a4dd7edf5f50c4fe79592b9bd638125aa
Cao Viết Huy <hieppro138@gmail.com>
2023-12-21 08:03:42 +0700

    first commit

---
+++ P1/Bacteria.swift
@@ -0,0 +1,8 @@
+//
+//  Bacteria.swift
+//  P1
+//
+//  Created by Cao Viet Huy on 20/12/2023.
+//
+
+import Foundation
---
+++ P1/BacteriaView.swift
@@ -0,0 +1,20 @@
+//
+//  BacteriaView.swift
+//  P1
+//
+//  Created by Cao Viet Huy on 20/12/2023.
+//
+
+import SwiftUI
+
+struct BacteriaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BacteriaView_Previews: PreviewProvider {
+    static var previews: some View {
+        BacteriaView()
+    }
+}
